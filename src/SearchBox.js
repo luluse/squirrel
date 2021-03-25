a186c2d417f72e9cfaabb28c315c5d17e01bfc48
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-03-25 12:59:20 -0700

    added state for searchbox

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({searchChange}) => {
   return(
     <div className='pa2'>
     <input 
     className='pa3 ba b--light-green bg-washed-green'
     type='search' 
     placeholder='search friends' 
+    onChange={searchChange}
     />
     </div>
   )
