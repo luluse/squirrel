f372ae005819c2c46a94572fc04edceaf505aa8f
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-03-25 12:21:08 -0700

    added search box

---
+++ src/SearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBox = () => {
+  return(
+    <div className='pa2'>
+    <input 
+    className='pa3 ba b--light-green bg-washed-green'
+    type='search' 
+    placeholder='search friends' 
+    />
+    </div>
+  )
+}
+
+export default SearchBox;
