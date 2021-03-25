20b13beeba3e14a3f918e7d6596d1c43ff064acb
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-03-25 10:50:05 -0700

    created card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className='bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img src='https://picsum.photos/200' alt='jordi'/>
+      <div>
+        <h2>Jordi</h2>
+        <p>jordi@gmail.com</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
