cb4d9466d53e9263ddc06d709ac0d051b2bf5df5
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-03-30 10:54:25 -0700

    added provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { createStore } from 'redux';
+import { searchRobots } from './reducers';
 
+const store = createStore(searchRobots);
 
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store={store}>
   <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
